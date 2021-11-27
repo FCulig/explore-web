38d99b52ba42b3b62784b3d32914e2b2c6a1b869
FCulig <filip.culig@gmail.com>
2021-11-27 21:15:57 +0100

    Added toast message to error interceptor

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -8,18 +8,26 @@ import {
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AuthenticationService } from '../services/authentication.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private toastr: ToastrService,
+  ) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
-      if (err.status === 401) {
+      if (err.error.reason && err.error.reason != "") {
+        this.toastr.error(err.error.reason);
+      }
+
+      /*if (err.status === 401) {
         this.authenticationService.logout();
         location.reload();
-      }
+      }*/
 
       const error = err.error.message || err.statusText;
       return throwError(error);
